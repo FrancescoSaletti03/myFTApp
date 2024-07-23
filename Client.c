0d09504c2adfa82365bb11d7e23558d43c901c4b
Francesco Saletti <saletti.2067545@studenti.uniroma1.it>
2024-07-23 16:17:14 +0200

    Parsing Client

--- Client.c
+++ Client.c
@@ -9,17 +9,55 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h>
-#define PORT 8080
-int main(int argx, char *argv[])
+#include <string.h>
+int main(int argc, char *argv[])
 {
+    int boolWrite = 0,boolRead = 0;
+    char *indirizzoIP = NULL, *porta = NULL, *from = NULL, *on = NULL;
+    if(!(argc == 10 || argc == 8))
+    {
+        printf("formato non valido \n");
+        exit(EXIT_FAILURE);
+    }
+
+    for(int i = 0; i < argc; i = i + 1)
+    {
+        if(strcmp(argv[i],"-w") == 0 && (i+1 >= argc || argv[i+1][0] == '-')) { boolWrite = 1; }
+        else if (strcmp(argv[i],"-r") == 0 && (i+1 >= argc || argv[i+1][0] == '-')) { boolRead = 1; }
+        else if (strcmp(argv[i],"-a") == 0 && i+1 < argc && argv[i+1][0] != '-') { indirizzoIP = argv[i+1]; }
+        else if (strcmp(argv[i],"-p") == 0 && i+1 < argc && argv[i+1][0] != '-') { porta = argv[i+1]; }
+        else if (strcmp(argv[i],"-f") == 0 && i+1 < argc && argv[i+1][0] != '-') { from = argv[i+1]; }
+        else if (strcmp(argv[i],"-o") == 0 && i+1 < argc && argv[i+1][0] != '-') { on = argv[i+1]; }
+        else if(argv[i][0] != '-' && (i+1 >= argc || argv[i+1][0] == '-')) {}
+        else
+        {
+            printf("formato non valido \n");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    if(on == NULL)
+    {
+        on = from;
+    }
+
+
     int client_socket,stato;
     char stringa[] = "Ciao";
     struct sockaddr_in sAddress;
 
     socklen_t aAddresslen = sizeof(sAddress);
     sAddress.sin_family=AF_INET;
-    sAddress.sin_port=htons(PORT);
-    if(inet_pton(AF_INET,"127.0.0.1",&sAddress.sin_addr) <=0)
+    char *check;
+    u_long portaInt = strtoll(porta,&check,10);
+    if(*check != '\0'){
+        perror("porta non valida");
+        exit(EXIT_FAILURE);
+    }
+
+    sAddress.sin_port=htons(portaInt);
+
+    if(inet_pton(AF_INET,indirizzoIP,&sAddress.sin_addr) <=0)
     {
         printf("ip non valido");
         return -1;
